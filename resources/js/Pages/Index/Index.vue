3731bc25e00d707da4e5498f2c2d3ce96f41b036
olek1305 <olek1305@gmail.com>
2024-01-08 23:20:09 +0100

    add listings model, factory, some packages composer&npm, vue pages&routes

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
+  <div>
+    The Message is: {{ message }}
+  </div>
+</template>
+
+<script setup>
+import { Link } from  '@inertiajs/vue3'
+defineProps({
+  message: String,
+})
+
+</script>
+
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Show</div>
+  <Link href="/">Home Page</Link>
+</template>
+
+<script setup>
+import { Link } from  '@inertiajs/vue3'
+
+</script>
+
+
