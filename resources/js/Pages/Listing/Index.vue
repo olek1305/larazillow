2535933d105f08350ca60f869286bb46cfa5e728
olek1305 <olek1305@gmail.com>
2024-01-09 23:03:00 +0100

    fix Mass Assignment, Resource Controller and Route model Binding

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing"/>
+        </Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from  '@inertiajs/vue3'
+import ListingAddress from "@/Pages/Components/ListingAddress.vue";
+
+defineProps({
+    listings: Array,
+})
+
+</script>
+
+
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ListingAddress :listing="listing"/>
+    </div>
+</template>
+
+<script setup>
+import ListingAddress from "@/Pages/Components/ListingAddress.vue";
+
+defineProps({
+    listing: Object,
+})
+
+</script>
+
+
