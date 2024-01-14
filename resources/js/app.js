9ca43ac9c82608a9efa095924243a9f2f1d0f20d
olek1305 <olek1305@gmail.com>
2024-01-14 11:23:12 +0100

    progress page, UI error

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,23 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
 import '../css/app.css'
+import NProgress from 'nprogress'
+import { router } from '@inertiajs/vue3'
+
+router.on('start', () => NProgress.start())
+router.on('finish', () => NProgress.done())
 
 createInertiaApp({
+    progress: {
+        color: 'green',
+        showSpinner: false
+    },
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     let page = pages[`./Pages/${name}.vue`]
     page.default.layout = page.default.layout || MainLayout
     return page
+
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
