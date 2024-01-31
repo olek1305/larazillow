4d71af7ca34ba44f33d1cc1308d891ed8c769a07
olek1305 <olek1305@gmail.com>
2024-01-31 20:53:55 +0100

    add RealtorListingImage

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
@@ -30,6 +31,11 @@ class Listing extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'by_user_id');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->HasMany(ListingImage::class);
     }
 
     public function scopeMostRecent(Builder $query): Builder
