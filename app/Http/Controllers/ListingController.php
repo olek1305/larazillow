63a76153a64d52845e351b3a2580e3db67ef2cbc
olek1305 <olek1305@gmail.com>
2024-02-02 18:59:31 +0100

    add show images

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -31,6 +31,8 @@ class ListingController extends Controller
 
     public function show(Listing $listing)
     {
+        $listing->load(['images']);
+
         return inertia('Listing/Show',
             [
                 'listing' => $listing
--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -33,7 +33,7 @@ class RealtorListingImageController extends Controller
         return redirect()->back()->with('success', 'Images uploaded!');
     }
 
-    public function destroy(Listing $listing, ListingImage $image)
+    public function destroy(ListingImage $image)
     {
         Storage::disk('public')->delete($image->filename);
         $image->delete();
