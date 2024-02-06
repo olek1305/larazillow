339a9c9e82c5fde88cb1c5ca97829912c8675021
olek1305 <olek1305@gmail.com>
2024-02-06 22:33:05 +0100

    fix css responsive

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full py-2">
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
         <div class="container mx-auto">
             <nav class="px-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
@@ -9,18 +9,29 @@
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
                 <div v-if="user" class="flex items-center gap-4">
-                    <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')">{{ user.name }}</Link>
-                    <Link :href="route('realtor.listing.create')"
-                          class="btn-primary">➕ New Listing
+                    <Link
+                        class="text-sm text-gray-500"
+                        :href="route('realtor.listing.index')"
+                    >
+                        {{ user.name }}
+                    </Link>
+                    <Link
+                        class="btn-primary"
+                        :href="route('realtor.listing.create')"
+                    >
+                        ➕ New Listing
                     </Link>
                     <div>
-                        <Link :href="route('logout')"
-                              method="delete"
-                              as="button"
-                        >Logout</Link>
+                        <Link
+                            :href="route('logout')"
+                            method="delete"
+                            as="button"
+                        >
+                            Logout
+                        </Link>
                     </div>
                 </div>
-                <div v-else class="flex items-center gap-4">
+                <div v-else class="flex items-center gap-2">
                     <Link :href="route('user-account.create')">Register</Link>
                     <Link :href="route('login')">Sign-In</Link>
                 </div>
