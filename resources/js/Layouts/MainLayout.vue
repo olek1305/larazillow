ade842a83a3a709fcad6ffce4918f56e8bff885d
olek1305 <olek1305@gmail.com>
2024-02-07 11:08:08 +0100

    fix css

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,9 +2,9 @@
     <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
         <div class="container mx-auto">
             <nav class="px-4 flex items-center justify-between">
-                <div class="text-lg font-medium">
-                    <Link :href="route('listing.index')">Listings</Link>&nbsp;
-                </div>
+<!--                <div class="text-lg font-medium">-->
+<!--                    <Link :href="route('listing.index')">Home</Link>&nbsp;-->
+<!--                </div>-->
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
