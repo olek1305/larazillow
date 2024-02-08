3b843f1366a6df0dfbdfac3264ce59efa014e350
olek1305 <olek1305@gmail.com>
2024-02-08 11:25:04 +0100

    fix no images, fix opacity button

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -3,10 +3,24 @@
         <template #header>Upload New Images</template>
         <form @submit.prevent="upload">
             <section class="flex items-center gap-2 my-4">
-                <input class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:horver:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
-                    type="file" multiple @input="addFiles"/>
-                <button type="submit" class="btn-outline disabled: opacity-25 disabled:cursor-not-allowed" :disabled="!canUpload">Upload</button>
-                <button type="reset" class="btn-outline" @click="reset">Reset</button>
+                <input
+                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:horver:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+                    type="file" multiple @input="addFiles"
+                />
+                <button
+                    type="submit"
+                    class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
+                    :disabled="!canUpload"
+                >
+                    Upload
+                </button>
+                <button
+                    type="reset"
+                    class="btn-outline"
+                    @click="reset"
+                >
+                    Reset
+                </button>
             </section>
             <div v-if="imageErrors.length" class="input-error">
                 <div v-for="(error, index) in imageErrors" :key="index">
@@ -19,9 +33,10 @@
     <Box v-if="listing.images.length" class="mt-4">
         <template #header>Current Listing Images</template>
         <section class="mt-4 grid grid-cols-3 gap-4">
-            <div v-for="image in listing.images"
-                 :key="image.id"
-                 class="flex flex-col justify-between"
+            <div
+                v-for="image in listing.images"
+                :key="image.id"
+                class="flex flex-col justify-between"
             >
                 <img :src="image.src" class="rounded-md" alt="image"/>
                 <Link
