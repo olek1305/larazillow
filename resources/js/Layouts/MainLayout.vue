940f272b4a5d760ad566bb90f62346b05c0cfbe4
olek1305 <olek1305@gmail.com>
2024-02-08 20:55:18 +0100

    change "icon"

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full py-2">
         <div class="container mx-auto">
             <nav class="px-4 flex items-center justify-between">
 <!--                <div class="text-lg font-medium">-->
@@ -19,7 +19,7 @@
                         class="btn-primary"
                         :href="route('realtor.listing.create')"
                     >
-                        ➕ New Listing
+                        + New Listing
                     </Link>
                     <div>
                         <Link
