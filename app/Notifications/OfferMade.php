146d72a55714011e98bc2432513b07629b96957c
olek1305 <olek1305@gmail.com>
2024-02-14 15:52:02 +0100

    add VerifyEmail, add resend VerifyEmail, add MailHog

--- app/Notifications/OfferMade.php
+++ app/Notifications/OfferMade.php
@@ -30,7 +30,7 @@ class OfferMade extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -42,8 +42,10 @@ class OfferMade extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    ->line("New offer ({$this->offer->amount}) was made for your listing")
+                    ->action('See your Listing',
+                        route('realtor.listing.show', ['listing' => $this->offer->listing_id])
+                    )
                     ->line('Thank you for using our application!');
     }
 
